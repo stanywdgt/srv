0e14804e06a236d80e090b4b32a5cbbb9d5ca69b
stanywdgt <142720215+stanywdgt@users.noreply.github.com>
2023-08-20 12:48:12 +0300

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    HOST: "dpg-cjgtvlj6fquc73ajeh40-a",
+    USER: "grady",
+    PORT: 5432,
+    PASSWORD: "WEW2QYGGA07AGGQi9TfP7g4o0x6QRChd",
+    DB: "stanysrv",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+//
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PORT: 5432,
+//     PASSWORD: "postgres",
+//     DB: "boomboom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
